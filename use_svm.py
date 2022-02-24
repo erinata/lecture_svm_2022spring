de23f68ef6cac5f99383c5485767e00575f369b7
Chungsang Tom Lam <erinata@gmail.com>
2022-02-24 11:48:40 -0500

    run svm linear

--- use_svm.py
+++ use_svm.py
@@ -4,6 +4,8 @@ import pandas
 
 from matplotlib import pyplot
 
+from sklearn import svm
+
 dataset = pandas.read_csv("datasets/dataset_svm_1.csv")
 
 print(dataset)
@@ -13,10 +15,26 @@ data = dataset.iloc[:,1:3].values
 
 # print(target)
 # print(data)
-
-
 pyplot.scatter(data[:,0], data[:,1], c=target)
 pyplot.savefig("scatterplot_1.png")
 pyplot.close()
+
+r2_scores, accuracy_scores, confusion_matrices = kfold_template.run_kfold(data, 
+																		target, 
+																		4, 
+																		svm.SVC(kernel="linear"), 
+																		1, 
+																		1)
+
+print(r2_scores)
+print(accuracy_scores)
+for i in confusion_matrices:
+	print(i)
+
+
+
+
+
+
 
 
